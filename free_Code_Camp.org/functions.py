08aaf62c40a6844731d354f30adda6c09a9d6856
cking <cassandra.king4@my.jcu.edu.au>
2019-12-18 11:54:56 +1100

    add practice with functions and fix formats

--- free_Code_Camp.org/functions.py
+++ free_Code_Camp.org/functions.py
@@ -3,6 +3,14 @@ Working with functions
 by; Cassandra King
 Date; 18/12/19
 """
-# a function that says hi to the user
-def say_hi():
-    print("hi user")
+
+
+# a function that says hello to the user
+def say_hello(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+
+say_hello("Jim", 76)
+say_hello("Mike", 35)
+
+# Return Statements
