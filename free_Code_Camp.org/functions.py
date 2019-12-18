129480660b1a1a4c5e9bc65ad5558b7e63375466
cking <cassandra.king4@my.jcu.edu.au>
2019-12-18 14:20:43 +1100

    practice functions and if statements

--- free_Code_Camp.org/functions.py
+++ free_Code_Camp.org/functions.py
@@ -13,4 +13,25 @@ def say_hello(name, age):
 say_hello("Jim", 76)
 say_hello("Mike", 35)
 
+
 # Return Statements
+# cubing numbers function
+def cube(num):
+    return num * num * num
+
+
+result = cube(3)
+print(result)
+
+# Working with If statements and simple boolens
+is_male = True
+is_tall = True
+if is_male and is_tall:
+    print("You are a tall Male")
+elif is_male and not is_tall:
+    print("You are a short male")
+elif not is_male and is_tall:
+    print("You are not a male but are tall")
+else:
+    print("You are not a Male and tall")
+# If statements and comparisons
