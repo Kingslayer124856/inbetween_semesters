2271c493523eca3d53ba598e8e884ca0b230d80b
cking <cassandra.king4@my.jcu.edu.au>
2019-12-18 16:06:04 +1100

    practice with dictionaries

---
+++ free_Code_Camp.org/dictonaries.py
@@ -0,0 +1,21 @@
+"""
+Dictonaries
+by; Cassandra King
+date; 18/12/19
+"""
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+print(monthConversions.get("", "Not a Valid Key"))
