e8e0809fa1c50bf171089f958d2eb725ca3c3aa2
cking <cassandra.king4@my.jcu.edu.au>
2020-01-02 22:49:03 +1100

    add For Loop examples

--- free_Code_Camp.org/loops.py
+++ free_Code_Camp.org/loops.py
@@ -30,3 +30,27 @@ while choice != 'Q':
         print(MENU)
         choice = input("Menu Choice").upper()
 breakpoint()
+
+# For Loop
+# Examples
+# printing each letter in string on its own line
+for letter in "Giraffe Academy":
+    print(letter)
+# in each iteration the list is added by the array
+friends = ["Jim", "Karen", "Kevin"]
+
+for friend in friends:
+    print(friend)
+# Printing a list from 0 to 10 not including 10 as it starts with 0 not 1
+for index in range(10):
+    print(index)
+# able to access the array using index
+len(friends)
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print("Not First")
