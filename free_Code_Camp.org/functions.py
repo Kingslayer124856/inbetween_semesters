983998b23e64d00fe8978f876d318c8db1e18f90
cking <cassandra.king4@my.jcu.edu.au>
2020-01-02 22:57:24 +1100

    add Exponent Function with a for loop

--- free_Code_Camp.org/functions.py
+++ free_Code_Camp.org/functions.py
@@ -47,3 +47,15 @@ def max_num(num1, num2, num3):
 
 
 print(max_num(300, 40, 5))
+
+
+# Exponent Function (2/1/20)
+# using a For Loop
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(3, 4))
