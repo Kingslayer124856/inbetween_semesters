ebc15e24a15b3e5f1dad911fcc4dc5af8908bff5
cking <cassandra.king4@my.jcu.edu.au>
2020-01-03 12:54:38 +1100

    Working with Modules

---
+++ free_Code_Camp.org/modules.py
@@ -0,0 +1,11 @@
+"""
+Modules
+By: Cassandra King
+Date: 3/1/20
+Note:
+Python modules list
+https://docs.python.org/3/py-modindex.html
+"""
+import useful_tools
+
+print(useful_tools.roll_dice(10))
---
+++ free_Code_Camp.org/useful_tools.py
@@ -0,0 +1,19 @@
+"""
+Useful tools
+By: Cassandra King
+Date: 3/1/20
+Note: Used for Modules.py
+"""
+import random
+
+feet_in_mile = 5280
+meters_in_kilometer = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1:]
+
+
+def roll_dice(num):
+    return random.randint(1, num)
