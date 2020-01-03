c861b3bf9e5530b87e6d4559df7c7f264950909f
cking <cassandra.king4@my.jcu.edu.au>
2020-01-03 13:09:57 +1100

    start practice with classes

---
+++ free_Code_Camp.org/classes.py
@@ -0,0 +1,14 @@
+"""
+Classes
+By: Cassandra King
+Date: 3/1/20
+Note:
+"""
+# Classes and Objects
+# student.py
+from student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.15, True)
+
+print(student2.gpa)
